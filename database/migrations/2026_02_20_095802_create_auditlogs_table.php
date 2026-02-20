a3b57fab3a544b044b1e32c87f9ba78c57ea7e1a
shegzbit <feranmiawotubo16@gmail.com>
2026-02-20 11:01:29 +0100

    FIX: add migration script for audit log models table setup

---
+++ database/migrations/2026_02_20_095802_create_auditlogs_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('auditlogs', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->string('name')->nullable();
+            $table->json('old_values')->nullable();
+            $table->json('new_values')->nullable();
+            $table->string('action')->nullable();
+            $table->timestamps();
+
+            $table->index('user_id');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('auditlogs');
+    }
+};
