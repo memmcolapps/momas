4ec3acb6927df3d420b7ff4ad62f31eac59e42c1
Awotubo Feranmi Gabriel <feranmiawotubo16@gmail.com>
2026-02-25 11:21:57 +0100

    FIX: push quick fix on verison management api - adds endpoint

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Estate\CustomerImportController as EstateCustomerImport
 use App\Http\Controllers\Estate\EstateController;
 use App\Http\Controllers\Feature\FeatureController;
 use App\Http\Controllers\Meter\MeterController;
+use App\Http\Controllers\NotificationController;
 use App\Http\Controllers\PosController;
 use App\Http\Controllers\Profile\ProfileController;
 use App\Http\Controllers\Service\ServiceController;
@@ -143,9 +144,7 @@ Route::group(['middleware' => ['auth:api', 'acess']], function () {
 });
 
 
-
-
-
+Route::get('check-app-version', [NotificationController::class, 'checkAppVersion']);
 
 
 
