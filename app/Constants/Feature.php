ccc6f604557adc41168f8b44329d431f77e8d022
Awotubo Feranmi Gabriel <feranmiawotubo16@gmail.com>
2026-04-22 14:05:07 +0100

    merging

---
+++ app/Constants/Feature.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Constants;
+
+class Feature
+{
+    public const MOMAS_METER = 'momas_meter';
+    public const OTHER_METER = 'other_meter';
+    public const PRINT_TOKEN = 'print_token';
+    public const ACCESS_TOKEN = 'access_token';
+    public const SERVICES = 'services';
+    public const BILL_PAYMENT = 'bill_payment';
+    public const SUPPORT = 'support';
+    public const TOP_UP = 'top_up';
+    public const ANALYSIS = 'analysis';
+}
