b93e3dd5fdde83dc67d01a86aee3ac2b4dab9e8d
Awotubo Feranmi Gabriel <feranmiawotubo16@gmail.com>
2026-04-22 15:06:05 +0100

    DEV: Accepeted constants

---
+++ app/Constants/TransactionConstants.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Constants;
+
+class TransactionConstants
+{
+    public const PAYMENT_PENDING = 0;
+    public const SERVICE_PENDING = 3;
+    public const PAYMENT_FAILED = 1;
+    public const TRANSACTION_COMPLETE = 2;
+}
