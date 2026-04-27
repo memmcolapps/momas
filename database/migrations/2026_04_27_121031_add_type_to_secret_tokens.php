5e7e3e76ccc54ef6de50a04595cbef67c3213e81
Awotubo Feranmi Gabriel <feranmiawotubo16@gmail.com>
2026-04-27 12:19:37 +0100

    DEV: add types to secret_tokens

--- database/migrations/2026_04_27_121031_add_type_to_secret_tokens.php
+++ database/migrations/2026_04_27_121031_add_type_to_secret_tokens.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('secret_tokens', function (Blueprint $table) {
-            //
+            $table->string('type')->nullable();
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('secret_tokens', function (Blueprint $table) {
-            //
+            $table->dropColumn('type');
         });
     }
 };
